322cfaf050b8fa5b3743751bcae2d3961fd0936b
Veggier <47649859+Veggier@users.noreply.github.com>
2019-10-04 14:13:27 -0400

    Update fancy-button.js

--- app/components/fancy-button.js
+++ app/components/fancy-button.js
@@ -1,10 +1,11 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  prop1: 'newValue',
+  /*prop1: 'newValue',
   actions: {
     update() {
       this.set('prop1', 'cake');
     }
   }
+  */
 });
